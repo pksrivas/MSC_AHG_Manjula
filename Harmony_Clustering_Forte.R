500c918e856406e956cf652f994ef77d72492700
ManjulaGurunath <mg2523@ic.ac.uk>
2024-08-28 02:33:51 +0100

    Update Harmony_Clustering_Forte.R

--- Harmony_Clustering_Forte.R
+++ Harmony_Clustering_Forte.R
@@ -51,71 +51,51 @@ Forte_Combined_transformed.harmony@meta.data
 
 cluster.markers <- FindAllMarkers(Forte_Combined_transformed.harmony)
 
-# B cells #9
-#CellTypist
 options(repr.plot.height = 15, repr.plot.width = 15)
 markergenes=c("Ms4a1","Cd79a","Cd19", "Hbg2", "Hba2", "Ptma", "Malat1", "Hba1","Cd74", "C1qa", "Cd68","Trem2","C1qa", "Ftl", "RNAse1", "Apoe", "Alf1", "Apoc1", "C1qa", "Ptprc","Igfbp1")
 FeaturePlot(object=Forte_Combined_transformed.harmony, reduction = "umap", label = TRUE, features=markergenes, order=TRUE, ncol = 2)
 VlnPlot(object=Forte_Combined_transformed.harmony, features=markergenes,ncol = 2 )
 
-# T_NK Cells #12
-#CellTypist, Forte
 options(repr.plot.height = 15, repr.plot.width = 15)
 markergenes=c("Nkg7","Gnly","Cd8a", "Prdm16", "Trdv2", "Snx29P1", "Linc01013", "Ms4a4b", "Gzma")
 FeaturePlot(object=Forte_Combined_transformed.harmony, reduction = "umap", label = TRUE, features=markergenes, order=TRUE, ncol = 2)
 VlnPlot(object=Forte_Combined_transformed.harmony, features=markergenes,ncol = 2 )
 
-#Macrophages #5,13
-#CellTypist
 options(repr.plot.height = 10, repr.plot.width = 15)
 markergenes=c("C1qa", "Cd68","Trem2","C1qa", "Ftl", "RNAse1", "Apoe", "Alf1", "Apoc1", "C1qa", "Ptprc","Igfbp1")
 FeaturePlot(object=Forte_Combined_transformed.harmony, reduction = "umap", label = TRUE, features=markergenes, order=TRUE, ncol = 2)
 VlnPlot(object=Forte_Combined_transformed.harmony, features=markergenes,ncol = 2 )
 
-#Monocyte #8
-#CellTypist, Forte
 options(repr.plot.height = 15, repr.plot.width = 15)
 markergenes=c("Kit", "Cpa3","Slc45a3","Ctag2", "Adcyap1", "Siglec17p", "Rps26p8", "Page5", "Chil3", "Plac8", "Saa3", "Arg1", "Cux1", "Ctsd", "Add3", "Ly6c2", "Plac8", "Ccr2")
 FeaturePlot(object=Forte_Combined_transformed.harmony, reduction = "umap", label = TRUE, features=markergenes, order=TRUE, ncol = 2)
 VlnPlot(object=Forte_Combined_transformed.harmony, features=markergenes,ncol = 2)
 
-# DC #17
-#CellTypist, Forte
 options(repr.plot.height = 10, repr.plot.width = 15)
 markergenes=c("Cd1c", "Fcer1a","Clec10a","Fcer1a", "Gpx1", "Rgs1", "Foxh1", "Cap2", "H2ab1", "Cd74")
 FeaturePlot(object=Forte_Combined_transformed.harmony, reduction = "umap", label = TRUE, features=markergenes, order=TRUE, ncol = 2)
 VlnPlot(object=Forte_Combined_transformed.harmony, features=markergenes,ncol = 2)
 
-# Granulocyte #11
-#CellTypist, Forte
 options(repr.plot.height = 20, repr.plot.width = 20)
 markergenes=c("Lyz", "Fcn1","Tyrobp","Ftl", "Igkc", "Trac", "Neat1", "Sat1", "Lyz", "Cotl1", "Chil3", "Plac8", "Saa3", "Arg1", "S100a8", "S100a9")
 FeaturePlot(object=Forte_Combined_transformed.harmony, reduction = "umap", label = TRUE, features=markergenes, order=TRUE, ncol = 2)
 VlnPlot(object=Forte_Combined_transformed.harmony, features=markergenes,ncol = 2)
 
-# Neutrophils
-#CellTypist
 options(repr.plot.height = 15, repr.plot.width = 15)
 markergenes=c("Lcn2", "Orm1","Mmp8","Page5", "Sele", "Sycp1", "Syn3", "Xcr1", "Defa3", "Ctag2")
 FeaturePlot(object=Forte_Combined_transformed.harmony, reduction = "umap", label = TRUE, features=markergenes, order=TRUE, ncol = 2)
 VlnPlot(object=Forte_Combined_transformed.harmony, features=markergenes,ncol = 2)
 
-# Endothelial #4,14
-#CellTypist
 options(repr.plot.height = 15, repr.plot.width = 15)
 markergenes=c("Cldn5", "Plvap","Sparcl1","Ramp3", "Sele", "Ackr1", "Adgrl4", "Plvap", "Cfi", "Mmrn2", "Meox2", "Fabp4", "Pecam1", "Lyve1", "Cldn5")
 FeaturePlot(object=Forte_Combined_transformed.harmony, reduction = "umap", label = TRUE, features=markergenes, order=TRUE, ncol = 2)
 VlnPlot(object=Forte_Combined_transformed.harmony, features=markergenes,ncol = 2)
 
-# Fibroblasts #0,1,2,6,7,10,15,16,19
-#CellTypist
 options(repr.plot.height = 30, repr.plot.width = 15)
 markergenes=c("Col1a1", "Col1a2","Dcn","Dpt", "Smoc2", "Sfrp1", "Prrx1", "Mxra5", "Ebf2", "Ntrk2", "Olfml1", "Srpx", "Glt8d2", "Gsn", "Dcn", "Wif1", "Dkk3", "Mt2", "Timp1")
 FeaturePlot(object=Forte_Combined_transformed.harmony, reduction = "umap", label = TRUE, features=markergenes, order=TRUE, ncol = 2)
 VlnPlot(object=Forte_Combined_transformed.harmony, features=markergenes,ncol = 2)
 
-#SMC #18
-#Forte
 options(repr.plot.height = 15, repr.plot.width = 15)
 markergenes=c("Rgs5", "Vtn", "Kcnj8", "Myl9")
 FeaturePlot(object=Forte_Combined_transformed.harmony, reduction = "umap", label = TRUE, features=markergenes, order=TRUE, ncol = 2)
